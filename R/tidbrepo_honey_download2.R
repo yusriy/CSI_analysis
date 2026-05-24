4adc4a30249e59fea66dfce70c67f704209969f4
Yusri <8579268+yusriy@users.noreply.github.com>
2026-05-25 00:01:05 +0800

    Delete R/tidbrepo_honey_download2.R

--- R/tidbrepo_honey_download2.R
+++
@@ -1,8 +0,0 @@
-library(httr2)
-library(readr)
-source("download_tidbrepo.R") # Source the download function first
-
-db_id <- "306d4236-97b2-4a17-a82d-957ac987afac"
-tb_id <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" # <----- Change this to the actual data table ID.
-
-df <- download_tidbrepo(db_id = db_id, tb_id = tb_id)
